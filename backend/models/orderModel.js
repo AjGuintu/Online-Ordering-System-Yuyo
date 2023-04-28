05ba9a5ebc248b056dfd526f77480dee023af53a
AJ <amyojosiah@gmail.com>
2023-04-28 13:47:10 +0800

    added more Order Status

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -38,8 +38,6 @@ const orderSchema = new mongoose.Schema(
     paidAt: { type: Date },
     isDelivered: { type: Boolean, default: false },
     deliveredAt: { type: Date },
-    isOutForDelivery: { type: Boolean, default: false },
-    outForDeliveryAt: { type: Date },
   },
   {
     timestamps: true,
